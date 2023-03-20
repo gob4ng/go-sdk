e7869ae5a423caa5919cb9e786eaf29ebd5c6765
h4lim <--add>
2023-03-20 13:01:57 +0700

    add config implementation

--- file/config.go
+++ file/config.go
@@ -1,1 +1,41 @@
 package file
+
+import (
+	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
+	"strings"
+)
+func NewConfig(fileName string) *error {
+
+	splits := strings.Split(filepath.Base(fileName), ".")
+	viper.SetConfigName(filepath.Base(splits[0]))
+	viper.AddConfigPath(filepath.Dir(fileName))
+
+	if err := viper.ReadInConfig();err != nil {
+		return &err
+	}
+
+	return nil
+}
+
+func MustGetString(key string) string {
+	checkKey(key)
+	return viper.GetString(key)
+}
+
+func MustGetInt(key string) int {
+	checkKey(key)
+	return viper.GetInt(key)
+}
+
+func MustGetBool(key string) bool {
+	checkKey(key)
+	return viper.GetBool(key)
+}
+
+func checkKey(key string) {
+	if !viper.IsSet(key) {
+		os.Exit(1)
+	}
+}
--- file/oss.go
+++ file/oss.go
@@ -1,12 +1,13 @@
 package file
 
 import (
-	"bitbucket.org/dab-ekyc/app/domains/types"
 	"encoding/json"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/sts"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
 type OssConfig struct {
@@ -15,7 +16,7 @@ type OssConfig struct {
 	AccessKeySecret string
 	BucketName      string
 	Directory       string
-	SecurityToken   string
+	StsData         StsData
 }
 
 type StsData struct {
@@ -23,11 +24,30 @@ type StsData struct {
 	Scheme          string
 	RoleArn         string
 	RoleSessionName string
-	ExpiredTime     int
+}
+
+type StsCredential struct {
+	SecurityToken string
+	ExpiredTime   int64
+}
+
+type ResponseSts struct {
+	RequestID       string `json:"RequestId"`
+	AssumedRoleUser struct {
+		Arn           string `json:"Arn"`
+		AssumedRoleID string `json:"AssumedRoleId"`
+	} `json:"AssumedRoleUser"`
+	Credentials struct {
+		SecurityToken   string    `json:"SecurityToken"`
+		AccessKeyID     string    `json:"AccessKeyId"`
+		AccessKeySecret string    `json:"AccessKeySecret"`
+		Expiration      time.Time `json:"Expiration"`
+	} `json:"Credentials"`
 }
 
 type OssFunction interface {
-	GetSts(data StsData) *error
+	GetSts(data StsData, expiredTime int64) (*StsCredential, *error)
+	GetUrlImage(fileName string, credential *StsCredential) (*string, *error)
 	Upload(directory string, fileName string, destinationFileName string) (*string, *error)
 	Download(fileName string, targetDownload string) (*string, *error)
 }
@@ -42,11 +62,11 @@ func NewOssConfig(config OssConfig) OssFunction {
 	}
 }
 
-func (o OssConfig) GetSts(data StsData) *error {
+func (o OssConfig) GetSts(data StsData, expiredTime int64) (*StsCredential, *error) {
 
 	stsClient, err := sts.NewClientWithAccessKey(data.Region, o.AccessKeyID, o.AccessKeySecret)
 	if err != nil {
-		return &err
+		return nil, &err
 	}
 
 	request := sts.CreateAssumeRoleRequest()
@@ -56,18 +76,50 @@ func (o OssConfig) GetSts(data StsData) *error {
 
 	assumeResponse, err := stsClient.AssumeRole(request)
 	if err != nil {
-		return &err
+		return nil, &err
 	}
 
-	var responseSts types.ResponseSts
+	var responseSts ResponseSts
 	if err := json.Unmarshal(assumeResponse.GetHttpContentBytes(), &responseSts); err != nil {
-		return &err
+		return nil, &err
 	}
 
-	o.SecurityToken = responseSts.Credentials.SecurityToken
+	stsCredential := StsCredential{
+		SecurityToken: responseSts.Credentials.SecurityToken,
+		ExpiredTime:   expiredTime,
+	}
 
-	return nil
+	return &stsCredential, nil
 
+}
+
+func (o OssConfig) GetUrlImage(fileName string, credential *StsCredential) (*string, *error) {
+
+	securityToken := ""
+	var expiredTime int64 = 0
+	if credential != nil {
+		securityToken = credential.SecurityToken
+		expiredTime = credential.ExpiredTime
+	}
+
+	client, err := oss.New(o.HostName, o.AccessKeyID, o.AccessKeySecret, oss.SecurityToken(securityToken))
+	if err != nil {
+		return nil, &err
+	}
+
+	bucket, err := client.Bucket(o.BucketName)
+	if err != nil {
+		return nil, &err
+	}
+
+	signedURL, err := bucket.SignURL(filepath.Join(o.Directory, fileName), oss.HTTPGet, expiredTime)
+	if err != nil {
+		return nil, &err
+	}
+
+	httpsUrl := strings.Replace(signedURL, "http", "https", 1)
+
+	return &httpsUrl, nil
 }
 
 func (o OssConfig) Upload(directory string, fileName string, destinationFileName string) (*string, *error) {
@@ -113,7 +165,8 @@ func (o OssConfig) Download(fileName string, targetDownload string) (*string, *e
 		return nil, &err
 	}
 
-	if err := bucket.GetObjectToFile(filepath.Join(o.Directory, fileName), filepath.Join(targetDownload, fileName)); err != nil {
+	if err := bucket.GetObjectToFile(filepath.Join(o.Directory, fileName),
+		filepath.Join(targetDownload, fileName)); err != nil {
 		return nil, &err
 	}
 
